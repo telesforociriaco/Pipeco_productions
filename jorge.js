586c8bda381a2aa84bf81c1852abacfeb85963bb
unknown <telesforociriaco@gmail.com>
2016-06-04 14:21:13 -0500

    tela_comes

---
+++ jorge.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var sillas = new Array("54","16","46","40","32","29");
+document.write("la sillas disponibles para el cliente en el cine es: "+sillas[0]+"<br><br>");
+
+
+document.write("Las sillas en orden numerco acendente son: "+sillas.sort()+"<br><br>");
+document.write("Las sillas en orden numerco decendente son: "+sillas.reverse()+"<br><br>");
